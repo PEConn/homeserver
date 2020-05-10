33c107e0ba627c522067c1cf36bb462ccb9a581c
Peter E Conn <conn.peter@gmail.com>
2020-05-10 16:31:40 +0100

    Put meal plan up.

---
+++ python/logic/model.py
@@ -0,0 +1,24 @@
+PLAN = ([
+    {
+        "day": "Sunday",
+        "meals": [
+            {"time": "Dinner", "meal": "Sweet & Sour Pork", "cook": "Kim"}
+        ]
+    },
+    {
+        "day": "Monday",
+        "meals": [
+            {"time": "Lunch", "meal": "Gnocchi bake", "cook": "Peter"},
+            {"time": "Dinner", "meal": "Pad Thai", "cook": "Kim"},
+        ]
+    },
+    {
+        "day": "Beyond",
+        "meals": [
+            {"time": "???", "meal": "Lentil & Sausage", "cook": "Peter"}
+        ]
+    }
+])
+
+def getPlan():
+    return PLAN
--- python/main.py
+++ python/main.py
@@ -1,12 +1,19 @@
 from bottle import Bottle, request, route, run, static_file
+import json
+
+from logic import model
 
 STATIC_PATH = '/home/peter/projects/homeserver/website'
 
 app = Bottle()
 
 @app.route('/api')
-def surprise():
+def root():
     return 'Some data...'
+
+@app.route('/api/mealplan')
+def meal_plan():
+    return json.dumps(model.getPlan())
 
 @app.route('/')
 @app.route('/<filename>')
